a48147fbb65d87917ec5bda70095e639eace5b24
Mike Christopher <mike.christopher@consensys.net>
2024-05-13 14:22:00 -0700

    skillsgpt

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -64,8 +64,8 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       },
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'SkillsGPT',
+        short_name: 'SkillsGPT',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
